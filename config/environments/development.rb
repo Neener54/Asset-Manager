c3d64c6ca6ac64380c4beae95206e79cd0b259d4
Ryan MacGillivray <rmacg89@gmail.com>
2012-06-22 21:26:31 +0100

    working confirmation using gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,17 @@ AssetManager::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    authentication: "plain",
+    user_name: "jtcfurniture@gmail.com",
+    password: "stephani3"
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
