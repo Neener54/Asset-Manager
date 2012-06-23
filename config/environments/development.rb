820e6a60f313ac3c6e6bb3841bd1ebd4bff611b5
Ryan MacGillivray <rmacg89@gmail.com>
2012-06-23 10:12:46 +0100

    replaced basic email with html version.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ AssetManager::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "192.168.1.64:3000" }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
