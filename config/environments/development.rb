1643ce3adbd552f2732dc866dc41bd8bef684b6d
Ryan MacGillivray <rmacg89@gmail.com>
2013-06-22 23:22:55 +0100

    trailing whitespace removal from dev generated files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ AssetManager::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['mail_server'],
